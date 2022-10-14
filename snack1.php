cad8de7feb7360b21b61ec2a164073f5adecca42
valentinaiori <valentinaiori@live.it>
2022-10-14 11:58:26 +0200

    Add improvements snack 1

--- snack1.php
+++ snack1.php
@@ -11,49 +11,49 @@ Olimpia Milano - Cantù | 55-60
 
 $basket_matches = [
     [
-        "homeTeam" => "EA7 EMPORIO ARMANI MILANO -",
+        "homeTeam" => "EA7 EMPORIO ARMANI MILANO",
         "opposingTeam" => "GIVOVA SCAFATI",
         "homeTeamPoints" => "66",
         "opposingTeamPoints" => "75",
     ],
     [
-        "homeTeam" => "GERMANI BRESCIA -",
+        "homeTeam" => "GERMANI BRESCIA",
         "opposingTeam" => "OPENJOBMETIS VARESE",
         "homeTeamPoints" => "88",
         "opposingTeamPoints" => "83",
     ],
     [
-        "homeTeam" => "DOLOMITI ENERGIA TRENTINO -",
+        "homeTeam" => "DOLOMITI ENERGIA TRENTINO",
         "opposingTeam" => "NUTRIBULLET TREVISO BASKET",
         "homeTeamPoints" => "66",
         "opposingTeamPoints" => "57",
     ],
     [
-        "homeTeam" => "BANCO DI SARDEGNA SASSARI -",
+        "homeTeam" => "BANCO DI SARDEGNA SASSARI",
         "opposingTeam" => "TEZENIS VERONA",
         "homeTeamPoints" => "101",
         "opposingTeamPoints" => "79",
     ],
     [
-        "homeTeam" => "HAPPY CASA BRINDISI -",
+        "homeTeam" => "HAPPY CASA BRINDISI",
         "opposingTeam" => "GEVI NAPOLI BASKET",
         "homeTeamPoints" => "77",
         "opposingTeamPoints" => "70",
     ],
     [
-        "homeTeam" => "VIRTUS SEGAFREDO BOLOGNA -",
+        "homeTeam" => "VIRTUS SEGAFREDO BOLOGNA",
         "opposingTeam" => "PALLACANESTRO TRIESTE",
         "homeTeamPoints" => "85",
         "opposingTeamPoints" => "80",
     ],
     [
-        "homeTeam" => "UNAHOTELS REGGIO EMILIA -",
+        "homeTeam" => "UNAHOTELS REGGIO EMILIA",
         "opposingTeam" => "BERTRAM YACHTS DERTHONA TORTONA",
         "homeTeamPoints" => "59",
         "opposingTeamPoints" => "63",
     ],
     [
-        "homeTeam" => "CARPEGNA PROSCIUTTO PESARO -",
+        "homeTeam" => "CARPEGNA PROSCIUTTO PESARO",
         "opposingTeam" => "UMANA REYER VENEZIA",
         "homeTeamPoints" => "90",
         "opposingTeamPoints" => "89",
@@ -62,19 +62,18 @@ $basket_matches = [
 ?>
 <h2>1° GIORNATA DI CAMPIONATO</h2>
 <?php
-    foreach ($basket_matches as $match) : ?>
+    foreach ($basket_matches as $match) {
+
+        ?>
+
         <div>
-            <?php 
-            echo $match["homeTeam"];
-            echo $match["opposingTeam"];
-           
-            ?>
-                <?php
-                if ($match["homeTeamPoints"] > $match["opposingTeamPoints"]) {
-                    echo  $match["homeTeamPoints"] . " - " . $match["opposingTeamPoints"];
-                } else {
-                    echo $match["homeTeamPoints"] . " - " . $match["opposingTeamPoints"];
-                }
-                ?>
+            <p><?= $match["homeTeam"] ?> - <?= $match["opposingTeam"] ?> | <?= $match["homeTeamPoints"] ?> - <?= $match["opposingTeamPoints"] ?></p>
+
+        </div>
+        <?php
+    }
+    ?>
+
+
         
-    <?php endforeach ?>
+                
